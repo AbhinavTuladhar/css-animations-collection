490f0629f3c60ab3b4a028bae21bc5b6d5b6a4f8
AbhinavTuladhar <tuladharabhinav78@gmail.com>
2024-12-15 15:57:35 +0545

    feat: install sass

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { resolve } from "path";
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxt/eslint"],
+  alias: {
+    "@": resolve(__dirname, "/"),
+  },
+  css: ["~/assets/scss/main.scss"],
 });
